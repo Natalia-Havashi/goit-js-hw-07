48ae9ea0b26d75653b384b381c74402e208bd2e6
Natalia-Havashi <natali.gavashi@gmail.com>
2023-04-08 21:39:31 +0300

    js7

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+
+function createGalleryItemsMarkup(items) {  `   `
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+  
+  function onImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG'){
+        return;
+    } 
+  
+  const instante = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width ="800" height="600">`,
+    {
+        onShow:(instance) => {
+            window.addEventListener("keydown", onEscapePress);
+        },
+        onClose: (instance) => {
+            window.removeEventListener("keydown", onEscapePress);
+        },
+    }
+  );
+   instante.show()
+  };
+
+  function onEscapePress(event){
+    if (event.code === "Escape"){
+        instance.close();
+    }
+  }
